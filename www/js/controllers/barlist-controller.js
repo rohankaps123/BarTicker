3a30310e892f2568c58afc76f8c1ff1d45f669ce
Emil Parikh <appdev.emil@gmail.com>
2016-10-08 17:12:03 -0400

    wired up views and controllers with static info

--- www/js/controllers/barlist-controller.js
+++ www/js/controllers/barlist-controller.js
@@ -1,2 +1,8 @@
 BarTicker.controller('barListController', function($scope,$window,$state,$location,BarService,PosService,MenuService) {
+    //$scope.allBars = BarService.[method]();
+    $scope.allBars = [
+        {id:1,name:"Rafters", direction:"down",menu:[{drink:"Well drink", cost:4},{drink:"Draft Beer", cost:5},{drink:"House Wine", cost:5}]},
+        {id:2,name:"McMurphy's", direction:"up", menu:[{drink:"Well drink", cost:7},{drink:"Draft Beer", cost:7},{drink:"House Wine", cost:8}]},
+        {id:3,name:"Monkey Bar", direction:"down", menu:[{drink:"Well drink", cost:5},{drink:"Draft Beer", cost:6},{drink:"House Wine", cost:6}]}
+    ];
 });
--- www/js/controllers/menu-controller.js
+++ www/js/controllers/menu-controller.js
@@ -1,3 +1,25 @@
-BarTicker.controller('barMenuController', function($scope,$stateParams,$location,BarService,PosService,MenuService) {
-  console.log("jhfhjjh");
+BarTicker.controller('barMenuController', function($scope,$stateParams,$location,BarService,PosService,MenuService) {       var bar;
+    var barId = $stateParams.id;
+	
+	console.log(barId);
+                                                                                                                     
+    var allBars = [
+        {id:1,name:"Rafters", direction:"down",menu:[{drink:"Well drink", cost:4, change:-0.05},{drink:"Draft Beer", cost:5,change:-0.02},{drink:"House Wine", cost:5,change:0}]},
+        {id:2,name:"McMurphy's", direction:"up", menu:[{drink:"Well drink", cost:7,change:0.07},{drink:"Draft Beer", cost:7,change:0.10},{drink:"House Wine", cost:8,change:0.20}]},
+        {id:3,name:"Monkey Bar", direction:"down", menu:[{drink:"Well drink", cost:5,change:0},{drink:"Draft Beer", cost:6,change:0.01},{drink:"House Wine", cost:6,change:0.02}]}
+    ];
+                                                                                                                     
+                                                                                                                     
+                                                                                                                     
+    for(var i=0; i < allBars.length; i++){
+        if(allBars[i].id == barId)
+            bar=allBars[i];
+    }                                                                                                       
+    $scope.bar = bar;
 });
+
+BarTicker.filter('percentage', ['$filter', function ($filter) {
+  return function (input, decimals) {
+    return $filter('number')(input * 100, decimals) + '%';
+  };
+}]);
