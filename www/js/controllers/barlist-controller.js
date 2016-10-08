36d172e289daf37757f4b6d97440f99149510c43
rohan kapoor <rohankaps123@gmail.com>
2016-10-08 17:15:59 -0400

    added nikhils

--- www/js/controllers/barlist-controller.js
+++ www/js/controllers/barlist-controller.js
@@ -1,2 +1,19 @@
-BarTicker.controller('barListController', function($scope,$window,$state,$location,BarService,PosService,MenuService) {
+BarTicker.controller('barListController', function($scope,$window,$state,$location,$interval,BarService,PosService,MenuService) {
+
+
+  var getIncreasingRate=function(orderRate,peopleRate,currentPrice,maxPrice){
+    return (orderRate+0.5*peopleRate)*0.02*currentPrice*(maxPrice-currentPrice)/maxPrice
+  };
+  var getDecreasingRate = function(orderRate,peopleRate,currentPrice){
+    return (orderRate+0.5*peopleRate)*0.02*currentPrice*(1-currentPrice)
+  };
+
+  var getCurrentPrice = function(orderRate,peopleRate,maxPrice,minPrice,startingPrice,time){
+    return minPrice + (maxPrice-minPrice)/(1+((((maxPrice-minPrice)/startingPrice)-1)*Math.exp(-(orderRate + 0.5*peopleRate)*0.02*time)))
+  };
+  var currentPrice = getCurrentPrice(50,10,100,4,4.000001,0.0003)
+  console.log("Current Price: " + currentPrice)
+  var x = getIncreasingRate(5,10,currentPrice,100);
+  console.log("Increasing Rate: " + x)
+
 });
--- www/js/controllers/menu-controller.js
+++ www/js/controllers/menu-controller.js
@@ -1,3 +1,16 @@
 BarTicker.controller('barMenuController', function($scope,$stateParams,$location,BarService,PosService,MenuService) {
-  console.log("jhfhjjh");
+
+var x = getIncreasingRate(5,10,getCurrentPrice(5,10,100,0.50,1,2));
+console.log("Increasing Rate: " + x)
+
+var getIncreasingRate=function(orderRate,peopleRate,currentPrice){
+  return (orderRate+peopleRate)*currentPrice*(1-currentPrice)
+};
+var getDecreasingRate = function(orderRate,peopleRate,currentPrice){
+  return (orderRate+peopleRate)*currentPrice*(1-currentPrice)
+};
+
+var getCurrentPrice = function(orderRate,peopleRate,maxPrice,minPrice,startingPrice,time){
+  return minPrice + (maxPrice-minPrice)/(1+(((maxPrice-minPrice)/startingPrice)-1)*expfun(-(orderRate + peopleRate)*time))
+};
 });
