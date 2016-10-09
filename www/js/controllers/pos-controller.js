2a0ffdf87777437e8674f41130a1f0d3ca204265
Emil Parikh <appdev.emil@gmail.com>
2016-10-09 00:41:56 -0400

    pos page init

--- www/js/controllers/pos-controller.js
+++ www/js/controllers/pos-controller.js
@@ -1,3 +1,22 @@
 BarTicker.controller('posController', function($scope,$stateParams,$state,$location,BarService,PosService,MenuService) {
-
+    $scope.allBars = [
+        {id:1,name:"Rafters", direction:"down",menu:[{drink:"Well drink", cost:4},{drink:"Draft Beer", cost:5},{drink:"House Wine", cost:5}]},
+        {id:2,name:"McMurphy's", direction:"up", menu:[{drink:"Well drink", cost:7},{drink:"Draft Beer", cost:7},{drink:"House Wine", cost:8}]},
+        {id:3,name:"Monkey Bar", direction:"down", menu:[{drink:"Well drink", cost:5},{drink:"Draft Beer", cost:6},{drink:"House Wine", cost:6}]}
+    ];
+    
+    $scope.selectedBarValue = -1;
+    
+    $scope.getSelectedBar = function(selectedBar) {
+        
+        $scope.selectedBarValue = selectedBar;
+      for(var i=0; i< $scope.allBars.length; i++)
+      {
+        if(selectedBar == $scope.allBars[i].id)
+            $scope.bar = $scope.allBars[i];
+      }
+  };
+    
+    
+    
 });
